70d8d085186eacdf1bf772011986ae1ce320c29d
Sandeep Thapa <sandeep.thapa@outlook.com>
2019-07-25 12:43:06 +0300

    Added redux-thunk middleware for get requests

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import jsonplaceholder from '../apis/jsonplaceholder';
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonplaceholder.get('/posts');
+
+    dispatch({
+        type: 'FETCH_POSTS',
+        payload: response.data
+    })
+
+};
