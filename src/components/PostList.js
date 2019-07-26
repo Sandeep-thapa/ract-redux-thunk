d17658daad8756067acaaea49e2c2da00328dc84
Sandeep Thapa <sandeep.thapa@outlook.com>
2019-07-26 10:05:03 +0300

    Updated Id bug

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -19,7 +19,7 @@ class PostList extends React.Component {
                             </h2>
                             <p>{post.body}</p>
                         </div>
-                        <UserHeader userId={post.userID} />
+                        <UserHeader userId={post.userId} />
                     </div>
 
                 </div>
